d3691c2a00518ad5973bdc5e27c628cb34a33e8e
Chnix17 <118142859+Chnix17@users.noreply.github.com>
2022-11-20 18:50:35 +0800

    Ordering system
    This system is a basic program that the user needs to input some order and it will total all the items together with the change.

---
+++ Ordering system.py
@@ -0,0 +1,22 @@
+print("""*************PHINMA SOUTH CANTEEN**************
+JUNKFOODS:           DRINKS:         
+BURGER - 30₱         COKE/ROYAL/SPRITE - 20₱
+FRIES - 15₱          WATER - 25₱""")
+customer_name = input("Please Enter your name: ")
+print("----Good day"
+,customer_name,"! "+"Welcome to PHINMA SOUTH CANTEEN!---- ")
+item1 = input("Please choose desire food do you want: ")
+food1 = int(input("Enter the food price: "))
+qty1 = int(input("Enter Qty of the food: "))   
+print("******Here is your order: ********")
+total1 = food1*qty1
+print(qty1,"x",item1,"-",total1,"₱") 
+print("your total amount is:",(total1),"₱")                      
+cash = int(input("Enter Cash to pay: "))
+print("ORDER BY: ",customer_name) 
+print("Item: ")
+print(qty1,"x",item1,"-",total1,"₱")
+print("Total: ",total1)  #Total of purchase
+print("Cash amount: ",cash) 
+print("Change: ",(cash-total1))
+print("   THANK YOU FOR PURCHASING!")
